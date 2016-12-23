80965ef7eab2a2a7a40e6017589b5d89631bcb14
Pavel Vovkotrub <markyarab97@gmail.com>
2016-12-24 00:11:42 +0200

    js toggle menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  $('.slider').slick();
-});
+var menuElem = document.getElementById('sweeties');
+var titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function () {
+    menuElem.classList.toggle('open');
+};
